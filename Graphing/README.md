a61d8b1c281a92cedb74358dbb7b2ebae8d0ad00
Dennis Phillip <dennis@critcomm.com>
2021-02-06 13:54:24 -0800

    Documentation: Added README file

---
+++ Graphing/README.md
@@ -0,0 +1,15 @@
+### Date created
+2-4-21
+
+### Project Title
+Weather Graphing Experimentation
+
+### Description
+This code started with a basic program to load csv data into MathPlotLib and will develop into an interactive anaylasis program that will pull weather data from online source using the Visual Crossing API
+
+### Files used
+graph_test.py
+example.csv
+
+### Credits
+The inital examples for graphing were taken from youtube Matplotlib Tutorial Matplotlib Tutorial 7 by sentdex: https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
--- Graphing/graph_test.py
+++ Graphing/graph_test.py
@@ -1,10 +1,9 @@
 # This is a test of Numpy Graphing
-import matplotlib.pyplot as plt
-import numpy as np
 import urllib
 import json
 import urllib.request
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
 
 #Sample 1
 '''import csv
@@ -24,24 +23,22 @@ plt.plot(x,y, label = 'loaded from file')'''
 '''x,y = np.loadtxt('example.csv',delimiter = ',', unpack=True)
 plt.plot(x,y, label = 'loaded from file')'''
 
+#Sample 3
 def graph_data(location):
-
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location + "?key=F3TTF62K96W72EA7MJS2CNL5V"
-
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-
-
-    print(data)
-
-
+    response = urllib.request.urlopen(url).read().decode()
+    weather_data = json.loads(response)
+    i = 0
+    while i < (len(weather_data['days'])):
+        print(str(weather_data['days'][i]['dew']) + '\n')
+        i += 1
 
 
 
 
+graph_data('San%20Diego')
 
-
-
+''' plt.plot(x,y, label = 'loaded from file')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Interesting Graph\nJust Kidding')
@@ -49,3 +46,4 @@ def graph_data(location):
     plt.show()
 
 graph_data(urllib.parse.quote('London, UK', safe=''))
+'''
