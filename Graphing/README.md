286f193b8cc82b0493655dd9d4e71c95a6778853
Dennis Phillip <dennis@critcomm.com>
2021-02-10 20:48:22 -0800

    doc: Updated README.MD to include data source in credits

--- Graphing/README.md
+++ Graphing/README.md
@@ -5,7 +5,8 @@
 Weather Graphing Experimentation
 
 ### Description
-This code started with a basic program to load csv data into MathPlotLib and will develop into an interactive anaylasis program that will pull weather data from online source using the Visual Crossing API
+This code started with a basic program to load csv data into MathPlotLib and will develop into an interactive anaylasis program that will pull weather data from online source using the Visual Crossing API.
+
 
 ### Files used
 graph_test.py
@@ -13,3 +14,4 @@ example.csv
 
 ### Credits
 The inital examples for graphing were taken from youtube Matplotlib Tutorial Matplotlib Tutorial 7 by sentdex: https://pythonprogramming.net/loading-file-data-matplotlib-tutorial/
+Weather Data is pulled from Visual Crossing https://www.visualcrossing.com using thier API
--- Graphing/graph_test.py
+++ Graphing/graph_test.py
@@ -26,28 +26,59 @@ plt.plot(x,y, label = 'loaded from file')'''
 plt.plot(x,y, label = 'loaded from file')'''
 
 #Sample 3 - using URL dataset in JSON format
+
+'''def bytespdate2num(fmt, encoding ='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+    def bytesconverter(b):
+        s =b.decode(encoding)
+        return strconverter(s)
+    return bytesconverter'''
+
+
+#Sample 4
+x = []
+y = []
 def graph_data(location):
+
+
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location + "?key=F3TTF62K96W72EA7MJS2CNL5V"
 #Read data from URL
     response = urllib.request.urlopen(url).read().decode()
 #Load JSON data into a library
     weather_data = json.loads(response)
-    i = 0
-    while i < (len(weather_data['days'])):
-        print(str(weather_data['days'][i]['dew']) + '\n')
-        i += 1
 
+    i = 0
+    while i < (len(weather_data['days'][0]['hours'])):
+        x.append((weather_data['days'][0]['hours'][i]['datetime']))
+        y.append((weather_data['days'][0]['hours'][i]['temp']))
+        i += 1
+    #print(weather_data['days'][0]['hours'][0]['temp'])
+
+#Sample 5
+'''def graph_data(location):
+
+
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location + "?key=F3TTF62K96W72EA7MJS2CNL5V"
+#Read data from URL
+    response = urllib.request.urlopen(url).read().decode()
+#Load JSON data into a library
+    weather_data = []
+    split_source = response.split('\n')
+
+
+
+    i = 0
+    while i < (len(weather_data['days'][0]['hours'])):
+        x.append((weather_data['days'][0]['hours'][i]['datetime']))
+        y.append((weather_data['days'][0]['hours'][i]['temp']))
+        i += 1'''
 
 
 
 graph_data('San%20Diego')
-
-''' plt.plot(x,y, label = 'loaded from file')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Interesting Graph\nJust Kidding')
-    plt.legend()
-    plt.show()
-
-graph_data(urllib.parse.quote('London, UK', safe=''))
-'''
+plt.plot(x,y, label = 'loaded from file')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nJust Kidding')
+plt.legend()
+plt.show()
