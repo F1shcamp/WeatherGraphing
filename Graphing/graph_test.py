d06bdaf37e5b6261d9e6981a35e2fa47c8c29cae
Dennis Phillip <dennis@critcomm.com>
2021-02-11 14:23:52 -0800

    feat:  Added user input to select city name
    User is prompted to enter a city name which is used as the location
    when calling the function

--- Graphing/graph_test.py
+++ Graphing/graph_test.py
@@ -6,7 +6,11 @@ import json
 import urllib.request
 from urllib.parse import quote
 import matplotlib.dates as mdates
+from matplotlib.dates import date2num
+from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
+
+
 
 #Sample 1
 '''import csv
@@ -41,7 +45,6 @@ x = []
 y = []
 def graph_data(location):
 
-
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location + "?key=F3TTF62K96W72EA7MJS2CNL5V"
 #Read data from URL
     response = urllib.request.urlopen(url).read().decode()
@@ -53,34 +56,18 @@ def graph_data(location):
         x.append((weather_data['days'][0]['hours'][i]['datetime']))
         y.append((weather_data['days'][0]['hours'][i]['temp']))
         i += 1
+
     #print(weather_data['days'][0]['hours'][0]['temp'])
 
-#Sample 5
-'''def graph_data(location):
 
-
-    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location + "?key=F3TTF62K96W72EA7MJS2CNL5V"
-#Read data from URL
-    response = urllib.request.urlopen(url).read().decode()
-#Load JSON data into a library
-    weather_data = []
-    split_source = response.split('\n')
-
-
-
-    i = 0
-    while i < (len(weather_data['days'][0]['hours'])):
-        x.append((weather_data['days'][0]['hours'][i]['datetime']))
-        y.append((weather_data['days'][0]['hours'][i]['temp']))
-        i += 1'''
-
-
-
-graph_data(quote('Salt Lake City'))
-
-plt.plot(x,y, label = 'loaded from file')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Interesting Graph\nJust Kidding')
+# get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+city_name = input('Please enter the name of a City (e.g. Las Vegas) for Temperature Data:  \n')
+graph_data(quote(city_name))
+figure(num=None, figsize=(16, 6), dpi=80, facecolor='w', edgecolor='k')
+plt.plot(x,y, label = 'loaded from Visual Crossing')
+plt.xlabel('Hour')
+plt.xticks(rotation=45)
+plt.ylabel('Temp in °F')
+plt.title('Temperature By Hour')
 plt.legend()
 plt.show()
